112b53b2d73dec55c3ecbf062ba7bcc86be8fc9c
radjiv carrere <radjiv.carrere@gmail.com>
2018-01-15 15:09:22 +1100

    just play around with vue

---
+++ src/components/Test.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="test">
+    <h1>{{title}}</h1>
+    <input type="text" v-model="title" /><br />
+    {{title}}
+
+    <p v-if="showName">
+       {{user.firstName}}{{user.lastname}}
+    </p>
+    <p v-else="showName">
+       Nobody
+    </p>
+
+    <ul>
+      <li v-for="item in items">
+        {{item.title}}
+      </li>
+    </ul>
+
+    <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit"/>
+    <hr />
+
+    <label>First name </label><input type="text" v-model="user.firstName" />
+
+    <br  />
+
+    <label>Last name </label><input type="text" v-model="user.lastName" />
+
+    <br  />
+
+    {{fullName}}
+
+    <hr />
+    {{msg}}
+
+    <button v-on:click="greet('HelloWorld')">Say gretting</button>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'test',
+    props: {
+      msg:{
+        type: String,
+        default:'Foobar'
+      }
+    },
+    data() {
+      return {
+        title: 'HelloWorld',
+        user:{
+          firstName: "john",
+          lastName: "Doe"
+        },
+        "showName": false,
+        items:[
+          {title:"item one"},
+          {title:"item two"},
+          {title:"item three"}
+        ]
+      }
+    },
+    methods:{
+      greet: function(greeting){
+        alert(greeting)
+      },
+      pressKey: function(e){
+          console.log(e.target.value)
+      },
+      enterHit: function(){
+        console.log("you hit enter");
+      }
+    },
+    computed: {
+      fullName:function(){
+        return this.user.firstName+ ' ' + this.user.lastName
+      }
+    }
+  }
+</script>
---
+++ src/components/Users.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="test">
+    <h1>Users</h1>
+    <form v-on:submit="addUser">
+      <input type="text" v-model="newUser.name" placeholder="enter name"  />
+      <br  />
+      <input type="text" v-model="newUser.email" placeholder="enter email"  />
+
+      <br  />
+      <input type="submit" value="Submit"  />
+
+    </form>
+
+    <ul>
+      <li v-for="user in users">
+        <input type="checkbox" class="toggle" v-model="user.contacted"  />
+        <span :class="{contacted: user.contacted}">
+          {{user.name}}: {{user.email}}
+          <button v-on:click="deleteUser(user)">x</button>
+        </span>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'Users',
+    data() {
+      return {
+        newUser: {},
+        users: [
+
+        ]
+      }
+    },
+    methods:{
+      addUser: function(e){
+        this.users.push({
+          name:this.newUser.name,
+          email:this.newUser.email
+        })
+        console.log('added')
+        e.preventDefault()
+      },
+      deleteUser: function(user) {
+          this.users.splice(this.users.indexOf(user), 1)
+      }
+    },
+    created:function(){
+      this.$http.get('https://jsonplaceholder.typicode.com/users')
+        .then(function(response){
+          this.users = response.data
+        })
+    }
+  }
+</script>
+
+<style scoped>
+  .contacted{
+    text-decoration :line-through;
+  }
+</style>
